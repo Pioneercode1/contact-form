70f8db8b9c29ec8b22b17cf14e1529d29575b923
pioneercode <pioneercode@outlook.com>
2025-10-18 18:11:51 +0200

    last commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+function validateForm(event) {
+    const form = event.target;
+    const requiredInputs = form.querySelectorAll('[required]');
+    const successMessage = document.getElementById('success-message');
+	
+    let isFormValid = true;
+	
+    requiredInputs.forEach(input => {
+        if (input.type !== 'radio' && input.type !== 'checkbox' && !input.value.trim()) {
+            isFormValid = false;
+        }
+        if (input.type === 'checkbox' && !input.checked) {
+            isFormValid = false;
+        }
+    });
+	
+    const radioButtons = form.querySelectorAll('input[name="query-type"]');
+    const isRadioChecked = Array.from(radioButtons).some(radio => radio.checked);
+	
+    if (!isRadioChecked) {
+        isFormValid = false;
+    }
+	
+    if (isFormValid) {
+        event.preventDefault();
+		
+        successMessage.style.display = 'block';
+        form.querySelector('h1').style.display = 'none';
+        form.querySelectorAll('.fieldset-info, .form-group, .fieldset-query-type, .div-checkbox-contacte, #btn-submit').forEach(el => {
+            el.style.display = 'none';
+        });
+		
+        return false; 
+		
+    } else {
+        console.log("Form is NOT valid. Browser will show default error messages.");
+        return true;
+    }
+}
